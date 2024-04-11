fecf16e466f3880aae3bf66f381258c619913986
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-04-11 14:23:12 +0200

    convertResult function added

--- js/script.js
+++ js/script.js
@@ -1,28 +1,33 @@
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-amount");
-let currencyElement = document.querySelector(".js-currency");
-let resultElement = document.querySelector(".js-result");
+{
+  const formElement = document.querySelector(".js-form");
+  const amountElement = document.querySelector(".js-amount");
+  const currencyElement = document.querySelector(".js-currency");
+  const resultElement = document.querySelector(".js-result");
 
-formElement.addEventListener("submit", (event) => {
+  const convertResult = (amount, currency) => {
+    const EURO = 4.34;
+    const USD = 4;
+    const GBP = 5;
+    switch (currency) {
+      case "EURO":
+        return amount * EURO;
+
+      case "USD":
+        return amount * USD;
+
+      case "GBP":
+        return amount * GBP;
+    }
+  };
+
+  formElement.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    let amount = amountElement.value;
-    let currency = currencyElement.value;
-    let result = resultElement.value;
-    let EURO = 4.34;
-    let USD = 4;
-    let GBP = 5;
-    
-    switch (currency) {
-        case "EURO":
-            result = (amount * EURO)
-            break;
-        case "USD":
-            result = (amount * USD)
-            break;
-        case "GBP":
-            result = (amount * GBP)
-            break;
-    };
+    const amount = amountElement.value;
+    const currency = currencyElement.value;
+
+    const result = convertResult(amount, currency);
+
     resultElement.innerText = result.toFixed(2);
-});
+  });
+}
