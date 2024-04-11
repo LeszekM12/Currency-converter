db71adbda51667dbd7e620caa601f8ae0a73c1c5
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-04-11 14:50:33 +0200

    modifyResule function added

--- js/script.js
+++ js/script.js
@@ -15,23 +15,30 @@
     }
   };
 
+  const modifyResultText = (result) => {
+    const resultElement = document.querySelector(".js-result");
+    resultElement.innerText = result.toFixed(2);
+  }
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const amountElement = document.querySelector(".js-amount");
+    const currencyElement = document.querySelector(".js-currency");
+
+    const amount = amountElement.value;
+    const currency = currencyElement.value;
+
+    const result = convertResult(amount, currency); 
+
+    modifyResultText(result);
+
+  };
+
   const init = () => {
     const formElement = document.querySelector(".js-form");
 
-    formElement.addEventListener("submit", (event) => {
-      event.preventDefault();
-
-      const amountElement = document.querySelector(".js-amount");
-      const currencyElement = document.querySelector(".js-currency");
-      const resultElement = document.querySelector(".js-result");
-
-      const amount = amountElement.value;
-      const currency = currencyElement.value;
-
-      const result = convertResult(amount, currency);
-
-      resultElement.innerText = result.toFixed(2);
-    });
+    formElement.addEventListener("submit", onFormSubmit);
   };
 
   init();
